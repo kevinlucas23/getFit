926989510c319f7538d6ccd51a677460a6e76e50
Brian Tran <briantran55@vt.edu>
2020-11-29 22:10:30 -0500

    added scale image

--- suggestions.cpp
+++ suggestions.cpp
@@ -6,6 +6,12 @@ Suggestions::Suggestions(QWidget *parent) :
     ui(new Ui::Suggestions)
 {
     ui->setupUi(this);
+    ui->title->setText("Weight Loss Food Tips");
+    QPixmap theScale("../getFit/scale.png");
+    int w = ui->scalePic->width();
+    int h = ui->scalePic->height();
+    ui->scalePic->setPixmap(theScale.scaled(w,h,Qt::KeepAspectRatio));
+
 }
 
 Suggestions::~Suggestions()
